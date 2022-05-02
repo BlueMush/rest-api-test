742bb3c8d1f42f75df25a42ce0f37fea2608aa22
BlueMush <gusah0909@naver.com>
2022-05-02 15:25:32 +0900

    Entity 코드 구현

---
+++ src/main/java/com/example/demo/events/Event.java
@@ -0,0 +1,33 @@
+package com.example.demo.events;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Event {
+	private String name;
+	private String description;
+	private LocalDateTime beginEnrollmentDateTime;
+	private LocalDateTime closeEnrollmentDateTime;
+	private LocalDateTime beginEventDateTime;
+	private LocalDateTime endEventDateTime;
+	private String location;		//	(optional)
+	private int basePrice;			//	(optional)
+	private int maxPrice;			//	(optional)
+	private int limitOfEnrollment;	//	(optional)
+	private boolean offline;
+	private boolean free;
+	private EventStatus eventStatus;
+	
+}
